c2c61f0e728d6e360b1ff038cf0001cf42a4244e
Maarten van Gompel <proycon@anaproy.nl>
2022-12-29 21:40:03 +0100

    added and fixes timetrack summary options

--- todo.actions.d/helpers/timetrack_sum.py
+++ todo.actions.d/helpers/timetrack_sum.py
@@ -4,6 +4,13 @@ import sys
 import fileinput
 from collections import defaultdict
 
+if len(sys.argv) > 1 and sys.argv[1] == "--all":
+    all = True
+    del sys.argv[1]
+else:
+    all = False
+
+
 counter = defaultdict(int)
 for line in fileinput.input():
     line = line.strip()
@@ -15,6 +22,8 @@ for line in fileinput.input():
     for field in fields:
         if field[0] in ('+','@'):
             counter[field] += duration
+    if all:
+        counter[" ".join(fields[1:])] += duration
 
-for key, value in counter.items():
-    print(key, value)
+for key, value in sorted(counter.items(),key=lambda x: -1 * int(x[1])):
+    print(value, key)
