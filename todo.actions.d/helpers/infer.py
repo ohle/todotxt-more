2d305c5c372bf64143dbd2a8ec080c8cc521abe1
Maarten van Gompel <proycon@anaproy.nl>
2023-01-02 20:37:28 +0100

    infer tags from project when adding new tasks

---
+++ todo.actions.d/helpers/infer.py
@@ -0,0 +1,20 @@
+import sys
+import json
+#maps projects to tags/contexts/projects, these will be added in addition to the project (rather than replacing it)
+#example: { "frog": ["@work","@huc","@clariah","+wp3t139"] }
+INFER_FROM_PROJECT = {}
+
+with open(sys.argv[1],'r',encoding='utf-8') as f:
+    INFER_FROM_PROJECT = json.load(f)
+
+taskline = " ".join(sys.argv[2:])
+
+projects = [ x for x in taskline.split(" ") if x and x[0] == '+' ]
+for project in projects:
+    project = project[1:]
+    if project in INFER_FROM_PROJECT:
+        for tag in INFER_FROM_PROJECT[project]:
+            if taskline.find(tag) == -1:
+                taskline += f" {tag}"
+
+
